ecbf97cd8eb1902823b378d2cd2c07625d8f5630
GANESH2903 <97089641+GANESH2903@users.noreply.github.com>
2022-05-22 17:15:42 +0000

    p12 in process

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,15 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
+import re
+data=open('http://py4e-data.dr-chuck.net/regex_sum_1551146.txt')
+sum=0
+y=[]
+for line in data:
+     line=line.rstrip()
+     x=re.findall('[0-9]+')
+     if len(x)>0:
+       #print(x)
+       for i in range of len(x):
+          y.appent(int(x[i]))   
+                  sum = sum+y[i]
+print('The sum of numbers in given data is' ,sum)
