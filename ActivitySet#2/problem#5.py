c657c2b5d4f29a4d12f39ac3e098ce9077e8fbd0
GANESH2903 <97089641+GANESH2903@users.noreply.github.com>
2022-06-06 18:23:49 +0000

    p5 doing...

--- ActivitySet#2/problem#5.py
+++ ActivitySet#2/problem#5.py
@@ -7,22 +7,35 @@ def get_cs():
 
 
 def cs_to_dict(cs):
-    """convert connect string to a dictionary"""
+  """convert connect string to a dictionary"""
+  x=cs.split(';')
+  ln=len(x)
+  for i in range(ln):
+    y=x[i].split('=')
+    t=list(y)
+    dct = {t[i]: t[i + 1] for i in range(0, len(t), 2)}
+  return dct
+  
 
 
 def dict_to_cs(d):
-    """convert a dictionary to connect string"""
+  """convert a dictionary to connect string"""
 
 
 def main():
-    cs = get_cs()
+  
+  cs = get_cs()
 
-    d = cs_to_dict(cs) # convert connect string to a dictionary
-    print(d)
+  d = cs_to_dict(cs) # convert connect string to a dictionary
+  print(d)
 
-    cs = dict_to_cs(d)
-    print(cs)
+  '''cs = dict_to_cs(d)
+  print(cs)'''
 
 
 if __name__ == '__main__':
-    main()
+  main()
+
+
+#  cd ActivitySet#2
+#  python problem#5.py
