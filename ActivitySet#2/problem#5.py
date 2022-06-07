6b2c27c2ca93bf7b704f2f06502911876d9eb7b6
GANESH2903 <97089641+GANESH2903@users.noreply.github.com>
2022-06-07 12:56:11 +0000

    p5 done

--- ActivitySet#2/problem#5.py
+++ ActivitySet#2/problem#5.py
@@ -1,41 +1,30 @@
-
-
+dic={}
 def get_cs():
-  #s = input('input a string')
-  s = system=s;database=d;username=u;password=p
-  return s
-
-
+    #s = input('input a string')
+    s = 'system=s;database=d;username=u;password=p'
+    return s
+  
 def cs_to_dict(cs):
-  """convert connect string to a dictionary"""
-  x=cs.split(';')
-  ln=len(x)
-  for i in range(ln):
-    y=x[i].split('=')
-    t=list(y)
-    dct = {t[i]: t[i + 1] for i in range(0, len(t), 2)}
-  return dct
-  
-
-
+    """convert connect string to a dictionary"""
+    x=cs.split(';')
+    for i in x:
+        y = i.split('=')
+        dic[y[0]] = y[1]
+    return dic
 def dict_to_cs(d):
-  """convert a dictionary to connect string"""
-
-
+    """convert a dictionary to connect string"""
+    str=""
+    for x in d:
+      # str=str+x+'='+d[x]+';'
+        str=';'.join(key +'='+ value for key, value in d.items())
+    return str
 def main():
-  
-  cs = get_cs()
-
-  d = cs_to_dict(cs) # convert connect string to a dictionary
-  print(d)
-
-  '''cs = dict_to_cs(d)
-  print(cs)'''
-
+    cs = get_cs()
+    d = cs_to_dict(cs)
+    print(d)
+    
+    cs = dict_to_cs(d)
+    print(cs)
 
 if __name__ == '__main__':
-  main()
-
-
-#  cd ActivitySet#2
-#  python problem#5.py
+    main()
