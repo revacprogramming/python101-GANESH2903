6fbff10157e243c03cf60c74ed09e2ff0387f531
GANESH2903 <97089641+GANESH2903@users.noreply.github.com>
2022-06-09 12:18:45 +0000

    p6 doing

--- ActivitySet#2/problem#6.py
+++ ActivitySet#2/problem#6.py
@@ -2,10 +2,8 @@
 
 class Menu:
     """fill in class definition here"""
-
-
-m = Menu()  # Menu is a class
-m.add("idly", 10)
+m = Menu()   # Menu is a class
+m.add("idly", 10)  
 
 m.add("vada", 20)
 
