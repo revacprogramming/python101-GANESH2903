c3c4d39e5e2d4589b829ca8e1c008f6034503757
GANESH2903 <97089641+GANESH2903@users.noreply.github.com>
2022-07-16 17:52:43 +0000

    p8

--- ActivitySet#2/problem#8.py
+++ ActivitySet#2/problem#8.py
@@ -1,13 +1,16 @@
 class Menu:
   def __init__(self,food,qty):
     self.food = food
-    
+    self.qty = qty
+  def __setattr__(self,food,qty):
+    self.__dict__[food]=qty
   
-  
 
 
-m = Menu()
-m["idly"] = 10
-m["vada"] = 20
+m = Menu("item",10)
+m.idly=30
+m.vada=20
+# m["idly"] = 10
+# m["vada"] = 20
 
-print(m)
+print(m.__dict__)
