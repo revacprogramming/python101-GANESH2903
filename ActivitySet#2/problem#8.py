6d4c829c689d7ea3bd0c431574686a2b5d9f08b7
GANESH2903 <97089641+GANESH2903@users.noreply.github.com>
2022-07-20 18:40:25 +0000

    p8

--- ActivitySet#2/problem#8.py
+++ ActivitySet#2/problem#8.py
@@ -1,16 +1,31 @@
 class Menu:
-  def __init__(self,food,qty):
-    self.food = food
-    self.qty = qty
-  def __setattr__(self,food,qty):
-    self.__dict__[food]=qty
+  def __init__(self):
+    pass
+
+
+m = Menu()
+m["idly"] = 10
+m["vada"] = 20
+
+print(m)
+
+
+
+
+
+# class Menu:
+#   def __init__(self,food,qty):
+#     self.food = food
+#     self.qty = qty
+#   def __setattr__(self,food,qty):
+#     self.__dict__[food]=qty
   
 
 
-m = Menu("item",10)
-m.idly=30
-m.vada=20
-# m["idly"] = 10
-# m["vada"] = 20
+# m = Menu("item",10)
+# m.idly=30
+# m.vada=20
+# # m["idly"] = 10
+# # m["vada"] = 20
 
-print(m.__dict__)
+# print(m.__dict__)
